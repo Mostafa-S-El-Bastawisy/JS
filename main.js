af3f7facf1379739fe79d8753833bedae64cc0e0
Mostafa S. El-bastawisy <mostafa.sayed112@gmail.com>
2021-07-15 14:24:04 +0200

    JavaScript-Bootcamp Assignment

---
+++ main.js
@@ -0,0 +1,32 @@
+document.write("<h1>El-Bastawisy Academy<h1/>");
+window.onload = function () {
+  document.querySelector("h1").style.color = "blue";
+  // document.querySelector("h1").style.font-size = 80 px;
+  // document.querySelector("h1").style.C;
+  // document.querySelector("h1").style.text-Align = center;
+  // document.querySelector("h1").style.font-family = Arial;
+}
+
+console.log("%cElzero %cWeb %cSchool", "color: red; font-size: 40px;", "color: Green; font-size: 40px; font-weight: Bolder;", "color: white; background-color:blue; font-size: 40px;")
+
+console.table(["Marwan", "Mostafa", "Ahmed", "Mohamed"])
+
+console.log("%cI'am In Console", "font-size: 70px; font-weight: bolder; color : blue;");
+
+document.write("I'am In Page");
+
+
+console.group("URL Details-1");
+console.log("Scheme: HTTPS");
+console.log("Host: example.com");
+console.group("URL Details-2");
+console.log("Scheme: HTTPS");
+console.log("Host: example.com");
+console.group("URL Details-3");
+console.log("Scheme: HTTPS");
+console.log("Host: example.com");
+console.groupEnd();
+console.group("URL Details");
+console.log("Scheme: HTTPS");
+console.log("Host: example.com");
+
